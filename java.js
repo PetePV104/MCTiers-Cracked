37e22d49faadc0871f300f2ab6b870cef38f6758
PetePV104 <paata.vardosanidze11@gmail.com>
2025-12-06 16:48:27 +0400

    1

--- java.js
+++ java.js
@@ -1,7 +1,9 @@
 // Demo Player Data (სრტიქტულად ის მოთამაშეები, რაც შენ მისაწოდებლად გქონდა)
 const players = [
   { name: "_BMW", tier: "HT3", points: 10 },
+  { name: "Pretty", tier: "LT3", points: 6 },
   { name: "Marlow_Geo", tier: "LT3", points: 6 },
+  { name: "MinTech", tier: "LT3", points: 6 },
   { name: "Red_Blu", tier: "LT4", points: 3 },
   { name: "_ItzRealUcha", tier: "HT5", points: 2 },
   { name: "WhiteBlack", tier: "HT5", points: 2 },
